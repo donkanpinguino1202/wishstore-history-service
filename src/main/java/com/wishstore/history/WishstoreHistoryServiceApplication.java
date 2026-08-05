9b3507bb93e168c06229eb390d4e5f6f056f2c05
unknown <donkanpinguino1202>
2026-08-05 13:24:01 -0500

    create repository

---
+++ src/main/java/com/wishstore/history/WishstoreHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wishstore.history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishstoreHistoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishstoreHistoryServiceApplication.class, args);
+	}
+
+}
