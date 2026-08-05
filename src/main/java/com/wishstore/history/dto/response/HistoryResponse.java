663a7a7a410c9bcb8638429d12491d702a1484af
donkanpinguino1202 <davisitho0530@gmail.com>
2026-08-05 17:27:28 -0500

    feat: add History response DTO

--- src/main/java/com/wishstore/history/dto/response/HistoryResponse.java
+++ src/main/java/com/wishstore/history/dto/response/HistoryResponse.java
@@ -1,4 +1,25 @@
 package com.wishstore.history.dto.response;
 
+import com.wishstore.history.enums.ActionType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class HistoryResponse {
+
+    private Long id;
+
+    private Long wishlistId;
+
+    private Long productId;
+
+    private ActionType action;
+
+    private String description;
+
+    private LocalDateTime createdAt;
+
 }
