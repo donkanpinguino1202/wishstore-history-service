bff924801a0a7a100f4208e7bbeae8988af9ff7d
donkanpinguino1202 <davisitho0530@gmail.com>
2026-08-05 17:29:07 -0500

    feat: implement HistoryRepository

--- src/main/java/com/wishstore/history/repository/HistoryRepository.java
+++ src/main/java/com/wishstore/history/repository/HistoryRepository.java
@@ -1,4 +1,19 @@
 package com.wishstore.history.repository;
 
-public interface HistoryRepository {
+import com.wishstore.history.entity.History;
+import com.wishstore.history.enums.ActionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface HistoryRepository extends JpaRepository<History, Long> {
+
+    List<History> findByWishlistId(Long wishlistId);
+
+    List<History> findByProductId(Long productId);
+
+    List<History> findByAction(ActionType action);
+
+    List<History> findByWishlistIdOrderByCreatedAtDesc(Long wishlistId);
+
 }
