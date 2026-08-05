67f89425e2afe07be74213c27268b0747961785b
donkanpinguino1202 <davisitho0530@gmail.com>
2026-08-05 17:22:52 -0500

    feat: add History entity

--- src/main/java/com/wishstore/history/entity/History.java
+++ src/main/java/com/wishstore/history/entity/History.java
@@ -1,4 +1,38 @@
 package com.wishstore.history.entity;
 
+import com.wishstore.history.enums.ActionType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "history")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class History {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "wishlist_id", nullable = false)
+    private Long wishlistId;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ActionType action;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
 }
